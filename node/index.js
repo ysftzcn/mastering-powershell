a829c92a92136fdcfb2672184dbbc52073efe886
ysftzcn <yusuftezcan@gmail.com>
2020-09-25 18:12:51 +0300

    ok

---
+++ node/index.js
@@ -0,0 +1,20 @@
+// console.log("Hello World!");
+
+const deger = 1;
+let deger2 = 3;
+
+// console.log("toplam : " + (deger + deger2));
+
+const array = [1, 2, 3, 4, 5];
+
+// for (let i = 0; array.length > i; i++) {
+//   console.log(array[i]);
+// }
+// console.log("Ben önceyim.");
+
+// TODO ES6 (2015)
+const obj = {
+  fırstName: "yusuf",
+};
+
+console.log(obj.fırstName);
